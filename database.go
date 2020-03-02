6467e9dedb7e1199a6dd963679bfb611310314ca
Matthew Kappus <matthew@AirShare.local>
2020-03-02 00:06:40 -0700

    add db.go examples

---
+++ database.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+// Database of users
+var Database *DB
+
+func init() {
+
+	db, err := sql.Open("sqlite3", "db/users.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	Database = &DB{db}
+}
+
+// DB implements CRUD for users db
+type DB struct {
+	*sql.DB
+}
+
+// User of db
+type User struct {
+	ID        int
+	FirstName string
+	LastName  string
+}
+
+// Get takes a first name and returns a user or error
+func (db *DB) Get(firstName string) (*User, error) {
+	stmt, err := db.Prepare("select * from users where first_name = ?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+	u := new(User)
+	err = stmt.QueryRow(firstName).Scan(&u.ID, &u.FirstName, &u.LastName)
+	return u, err
+}
--- main.go
+++ main.go
@@ -3,15 +3,46 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"time"
 )
 
+var sessionUser *User
+
 func greet(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World! %s", time.Now())
+	if sessionUser == nil {
+		http.Redirect(w, r, "/loginform", http.StatusTemporaryRedirect)
+		return
+	}
+	fmt.Fprintf(w, "<h1>Welcome %s</h1>", r.FormValue("name"))
+
+}
+
+func lf(w http.ResponseWriter, r *http.Request) {
+	// form takes a "name" and posts it to "/login"
+	fmt.Fprint(w, f)
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+
+	var err error
+	if sessionUser, err = Database.Get(r.FormValue("name")); err != nil {
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+	http.Redirect(w, r, "/", http.StatusPermanentRedirect)
 }
 
 func main() {
-	
+
+	http.HandleFunc("/loginform", lf)
+	http.HandleFunc("/login", login)
 	http.HandleFunc("/", greet)
+
 	http.ListenAndServe(":8080", nil)
 }
+
+const f = `
+<html><form method="POST" action="/login">
+	<input type="text" placeholder="First Name" name="name">
+	<input type="submit">
+</form></html>
+`
